9d486e7cdf4bd416ae3f0804f9839dcc8f40fad4
asitecn <asitecn@yahoo.com>
2016-08-06 09:01:38 -0700

    Create 168.Excel_Sheet_Column_Title.java

---
+++ leetcode/168.Excel_Sheet_Column_Title.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public String convertToTitle(int n) {
+        /*
+            1 -> A
+            2 -> B
+            3 -> C
+            ...
+            26 -> Z
+            27 -> AA
+            28 -> AB 
+        */
+        
+        StringBuilder sb = new StringBuilder();
+        while(n > 0) {
+            n--;
+            sb.insert(0, (char)(n % 26 + 'A'));
+            n /= 26;
+        }
+        return sb.toString();
+    }
+}
