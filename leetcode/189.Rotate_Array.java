aac996aece960669a1998e660ebc00ded5b5170a
asitecn <asitecn@yahoo.com>
2016-08-06 09:13:11 -0700

    Create 189.Rotate_Array.java

---
+++ leetcode/189.Rotate_Array.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            int temp = nums[i];
+            nums[i++] = nums[j];
+            nums[j--] = temp;
+        }
+    }
+    
+    public void rotate(int[] nums, int k) {
+        // For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+        if (nums.length == 0) {
+            return;
+        }
+        k %= nums.length;
+        
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, 0, nums.length - 1);
+    }
+}
