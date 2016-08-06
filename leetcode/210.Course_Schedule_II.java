31414ab06f76fa8b953594242a614eef26414360
asitecn <asitecn@yahoo.com>
2016-08-06 09:30:32 -0700

    Create 210.Course_Schedule_II.java

---
+++ leetcode/210.Course_Schedule_II.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> adjlist = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++) {
+            adjlist.add(new ArrayList<Integer>());
+        }
+        
+        for(int[] prerequisite : prerequisites) {
+            indegree[prerequisite[0]]++;
+            adjlist.get(prerequisite[1]).add(prerequisite[0]);
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        
+        int[] order = new int[numCourses];
+        int count = 0;
+        while(!queue.isEmpty()) {
+            Integer node = queue.poll();
+            order[count++] = node;
+            List<Integer> neighbors = adjlist.get(node);
+            for(Integer neighbor : neighbors) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        
+        return (count == numCourses ? order : new int[]{});        
+    }
+}
