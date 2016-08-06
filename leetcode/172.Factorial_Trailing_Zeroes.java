3436fa92f4162ab29cacf0a9ba5921dd588e1a43
asitecn <asitecn@yahoo.com>
2016-08-06 09:05:19 -0700

    Create 172.Factorial_Trailing_Zeroes.java

---
+++ leetcode/172.Factorial_Trailing_Zeroes.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int trailingZeroes(int n) {
+        
+        int sum = 0;
+        while(n > 1) {
+            sum += n / 5;
+            n /= 5;
+        }
+        return sum;
+    }
+}
