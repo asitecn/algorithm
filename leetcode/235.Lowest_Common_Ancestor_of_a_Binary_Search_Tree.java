dba0f746f8edcd00c09becb43d3a6e1bef24dfea
asitecn <asitecn@yahoo.com>
2016-08-06 22:30:52 -0700

    Create 235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java

---
+++ leetcode/235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == root || q == root) {
+            return root;
+        }
+        if (p == q) {
+            return p;
+        }
+        
+        while (root != null) {
+            if (root.val == p.val || root.val == q.val) {
+                return root;
+            }
+            if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            } else if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+        return null;
+    }
+}
