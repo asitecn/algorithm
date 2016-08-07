126204e2374ab51bcbca75ca958bc44bda50dbf9
asitecn <asitecn@yahoo.com>
2016-08-07 00:35:29 -0700

    Update 251.Flatten_2D_Vector.java

--- leetcode/251.Flatten_2D_Vector.java
+++ leetcode/251.Flatten_2D_Vector.java
@@ -1,3 +1,42 @@
+// Solution 1
+public class Vector2D {
+
+    Iterator<List<Integer>> iter1 = null;
+    Iterator<Integer> iter2 = null;
+
+    public Vector2D(List<List<Integer>> vec2d) {
+        if (vec2d != null && vec2d.size() > 0) {
+            iter1 = vec2d.iterator();
+            iter2 = iter1.next().iterator();
+            getNextValidRow();
+        }
+    }
+    
+    public void getNextValidRow() {
+        while(!iter2.hasNext() && iter1.hasNext()) {
+            iter2 = iter1.next().iterator();
+        }
+    }
+
+    public int next() {
+        int val = iter2.next();
+        getNextValidRow();
+        return val;
+    }
+
+    public boolean hasNext() {
+        return iter2 != null && iter2.hasNext();
+    }
+}
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D i = new Vector2D(vec2d);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
+
+
+// Solution 2
 public class Vector2D implements Iterator<Integer> {
 
     Iterator<List<Integer>> outer;
