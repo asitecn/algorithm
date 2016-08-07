fb2a78e8041efd637ed2fdf0f75f5af8ecd18bae
asitecn <asitecn@yahoo.com>
2016-08-07 00:59:13 -0700

    Update 268.Missing_Number.java

--- leetcode/268.Missing_Number.java
+++ leetcode/268.Missing_Number.java
@@ -1,3 +1,23 @@
+// Solution 1, 1ms
+public class Solution {
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public int missingNumber(int[] nums) {
+        int xor = 0;
+        for(int i = 0; i < nums.length; i++) {
+            xor ^= nums[i] ^ i;
+        }
+        xor ^= nums.length;
+        return xor;
+    }
+}
+
+
+// Solution 2, 3ms
 public class Solution {
     public void swap(int[] nums, int i, int j) {
         int temp = nums[i];
